890bc0095c0611af83b13728dcedbc3e585adafd
andresinpa <andresinfantepaez@gmail.com>
2023-05-09 18:35:42 -0500

    Cambios finales, apariencia y estilos de la interfaz, CRUD completo.

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.secret_key = "secret key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/productosbd'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://u3cxcelzhxt9xtos:DiEB2jQOnqztF6ubC6Gk@bupauath6phqcmgrvfr8-mysql.services.clever-cloud.com/bupauath6phqcmgrvfr8'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/productosbd'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
